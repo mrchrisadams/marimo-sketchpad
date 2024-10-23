84d5d5871a2811574a60b56763d38eb3590e3eb6
Chris Adams <chris@productscience.co.uk>
2024-10-23 13:25:35 +0200

    Tidy up the llama notebook
    After getting help in the Marimo Discord.

--- local-llm-with-ollama.py
+++ local-llm-with-ollama.py
@@ -20,18 +20,18 @@ app = marimo.App(width="medium")
 def __():
     import marimo as mo
     import llm
-    import markdown
-    import rich
-    return llm, markdown, mo, rich
+    return llm, mo
 
 
 @app.cell
 def __(mo):
     mo.md(
         """
-        ## For this to work, you need ollama installed, and to have pulled down the llama3.2 model
+        ## What's this? 
 
-        This is not too complicated though. If you visit  the [Ollama](https://ollama.com/) website, you can download the installer.
+        This is an interactive Marimo notebook, designed to use Ollama, for experimenting with generative AI using local models, and with a specific focus on small lanaguge models like Llama3.2 from Meta. 
+
+        You can use this notebook to connect to a local model run with Ollama, and run chat sessions.
 
         You then need to run the following command in your terminal to fetch the 2Gb model.
 
@@ -47,7 +47,7 @@ def __(mo):
 
 @app.cell
 def __(llm):
-    # list the models installed into this sandbox. Uncomment this to sanity check that you have access 
+    # list the models installed into this sandbox. Uncomment this to sanity check that you have access
     # to the necessary model
     # llm.get_models_with_aliases()
 
@@ -69,16 +69,14 @@ def __(mo):
 
         To use it, you need to pass in a callable, like a function that accepts a list of messages, and then returns a string of rendered text that represents the last response from the LLM.
 
-        So, that's what `rendered_chat()` below does.
-
-        While the returned text from the LLM is markdown, it still needs to be rendered. This is why we call the `markdown()` to turn the markdown text into html, to render on the page.
+        You can use the chat widget below - give it ago!
         """
     )
     return
 
 
 @app.cell
-def __(markdown, mo, model):
+def __(mo, model):
     conversation = model.conversation()
 
     def rendered_chat(messages, config):
@@ -87,10 +85,7 @@ def __(markdown, mo, model):
         Returns an llm.models.Response object
         """
         content = conversation.prompt(messages[-1].content)
-
-        # TODO: not every element in markdown appears to be rendered right
-        # see why this is in future
-        rendered_markdown = markdown.markdown(content.text())
+        rendered_markdown = mo.md(content.text())
 
         return rendered_markdown
 
